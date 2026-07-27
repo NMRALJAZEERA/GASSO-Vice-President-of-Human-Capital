83ff4aac05d6faa52171c77c4c5c76325707573f
NMRALJAZEERA <m.aljassser@gmail.com>
2026-07-28 01:32:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var saveBtn = document.getElementById("save-btn");
+saveBtn.addEventListener("click", function () {
+  // Get the contact information from the website
+  var contact = {
+    name: "Ibrahim Sulaiman Alotaieq",
+    phone: "0500887740",
+    email: "ialotaieq@gasco.com.sa"
+  };
+  // create a vcard file
+  var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nTEL;TYPE=work,voice:" + contact.phone + "\nEMAIL:" + contact.email + "\nEND:VCARD";
+  var blob = new Blob([vcard], { type: "text/vcard" });
+  var url = URL.createObjectURL(blob);
+  
+  const newLink = document.createElement('a');
+  newLink.download = contact.name + ".vcf";
+  newLink.textContent = contact.name;
+  newLink.href = url;
+  
+  newLink.click();
+});
+function test(){
+  alert('alala');
+}
